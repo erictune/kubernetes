99338f5035d6f149f40120171df4267cc9188e7c
Ryan Richard <ryanrichard07@gmail.com>
2015-03-11 22:05:35 -0500

    use $public_ipv4:6443 for apiservers
    Accessing the apiserver via read-only or https
    settings must be done via the public interface since
    that's where they listen and I don't see a way to move
    them to another elsewhere.
    
    The GCE implementation uses the https(port 6443) for
    this communication. We'll follow the same pattern but
    still need to implement a certificate to be served. Using
    the read-only port resulted in "failure to write" errors
    which makes sense.
    
    The python fixes were to force python2.7 since those
    calls don't work with python3+.

--- cluster/rackspace/util.sh
+++ cluster/rackspace/util.sh
@@ -58,12 +58,12 @@ verify-prereqs() {
 get-password() {
   local file="$HOME/.kubernetes_auth"
   if [[ -r "$file" ]]; then
-    KUBE_USER=$(cat "$file" | python -c 'import json,sys;print json.load(sys.stdin)["User"]')
-    KUBE_PASSWORD=$(cat "$file" | python -c 'import json,sys;print json.load(sys.stdin)["Password"]')
+    KUBE_USER=$(cat "$file" | python2.7 -c 'import json,sys;print json.load(sys.stdin)["User"]')
+    KUBE_PASSWORD=$(cat "$file" | python2.7 -c 'import json,sys;print json.load(sys.stdin)["Password"]')
     return
   fi
   KUBE_USER=admin
-  KUBE_PASSWORD=$(python -c 'import string,random; print "".join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(16))')
+  KUBE_PASSWORD=$(python2.7 -c 'import string,random; print "".join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(16))')
 
   # Store password for reuse.
   cat << EOF > "$file"
@@ -287,7 +287,7 @@ kube-up() {
   trap "rm -rf ${KUBE_TEMP}" EXIT
 
   get-password
-  python $(dirname $0)/../third_party/htpasswd/htpasswd.py -b -c ${KUBE_TEMP}/htpasswd $KUBE_USER $KUBE_PASSWORD
+  python2.7 $(dirname $0)/../third_party/htpasswd/htpasswd.py -b -c ${KUBE_TEMP}/htpasswd $KUBE_USER $KUBE_PASSWORD
   HTPASSWD=$(cat ${KUBE_TEMP}/htpasswd)
 
   rax-nova-network
